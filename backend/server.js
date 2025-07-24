658ac8f4f1fa2823cd5196c8619fddd81ba27eda
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-24 17:41:25 +0600

    created 4 of the endpoint

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,24 @@ import express from 'express';
 
 const app = express();
 
+// get endpoint
 app.get('/api/notes', (req, res) => {
-  res.send({ data: 'Have to walking to park' });
+  res.status(200).send({ data: 'Have to walking to park' });
+});
+
+// post endpoint
+app.post('/api/notes', (req, res) => {
+  res.status(201).json({ message: 'Note created Successfully!' });
+});
+
+// put endpoint
+app.put('/api/notes', (req, res) => {
+  res.status(200).json({ message: 'Note updated Successfully!' });
+});
+
+// delete endpoint
+app.put('/api/notes', (req, res) => {
+  res.status(200).json({ message: 'Note deleted Successfully!' });
 });
 
 app.listen(5001, () => {
