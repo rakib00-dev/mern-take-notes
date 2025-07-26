cdfbf5a65a311abb4625622b53ef16e128fa0b4d
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-26 22:09:19 +0600

    added card components

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,20 +4,21 @@ import toast from 'react-hot-toast';
 
 import Navbar from '../components/Navbar';
 import RateLimitedUI from '../components/RateLimitedUI';
+import NoteCard from '../components/NoteCard';
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-  const [note, setNote] = useState([]);
+  const [notes, setNotes] = useState([]);
 
   useEffect(() => {
     const fetchingData = async () => {
       try {
         const res = await axios.get('http://localhost:5001/api/notes');
-        console.log(res.data);
+        setNotes(res.data);
       } catch (error) {
         console.log('Error on data fetching', error);
-        if (error.response?.status === 429) {
+        if (error.response.status === 429) {
           setIsRateLimited(true);
           console.log(isRateLimited);
         } else {
@@ -34,6 +35,14 @@ const HomePage = () => {
     <div className="min-h-screen">
       <Navbar />
       {isRateLimited && <RateLimitedUI />}
+      <div className="grid grid-cols-1 mx-2 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-10">
+        {isLoading && (
+          <div className="text-center text-primary">Notes are loading...</div>
+        )}
+        {notes.length > 0 &&
+          !isRateLimited &&
+          notes.map((note) => <NoteCard key={note._id} note={note} />)}
+      </div>
     </div>
   );
 };
