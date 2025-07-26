4b770a299b4cec764fe29e4a62f204303414506e
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-26 18:23:40 +0600

    added pages and added the react-hot-toast and tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import HomePage from './pages/HomePage';
+import CreatePage from './pages/CreatePage';
+import NoteDetailPage from './pages/NoteDetailPage';
 
 const App = () => {
-  return <div>App</div>;
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <HomePage />,
+    },
+    {
+      path: '/create',
+      element: <CreatePage />,
+    },
+    {
+      path: '/note/:id',
+      element: <NoteDetailPage />,
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
 };
 
 export default App;
